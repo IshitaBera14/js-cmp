8c06ceaf6d5b1848802d11a0d202dc1b8ad4e87d
IshitaBera14 <156918112+IshitaBera14@users.noreply.github.com>
2024-11-11 06:14:51 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,17 +23,17 @@ let valueInNumber2 = Number(score3)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn) // true
+//console.log(booleanIsLoggedIn) // true
 
 let isLoggedIn2 = ""
 
 let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
-console.log(booleanIsLoggedIn2) // false
+//console.log(booleanIsLoggedIn2) // false
 
 let isLoggedIn3 = "Ishita"
 
 let booleanIsLoggedIn3 = Boolean(isLoggedIn3)
-console.log(booleanIsLoggedIn3) //true
+//console.log(booleanIsLoggedIn3) //true
 
 // 1 => true 0 => false
 // "" => false
@@ -42,5 +42,51 @@ console.log(booleanIsLoggedIn3) //true
 let no = 11
 
 let stringNo = String(no)
-console.log(stringNo) //11
-console.log(typeof stringNo) //string
+//console.log(stringNo) //11
+//console.log(typeof stringNo) //string
+
+
+
+
+/*********************** OPERATIONS **********************/
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/3)
+console.log(2%3) */
+
+let str1 = "hello"
+let str2 = " ishita"
+
+let str3  = str1 + str2
+//console.log(str3)
+
+/*console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2") */
+
+/*
+console.log(true)
+console.log(+true)
+//console.log(true+) //error
+console.log(+"") */
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
+
+let gameCounter2 = 100
+gameCounter2++;
+console.log(gameCounter2)
