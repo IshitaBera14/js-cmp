2b179245a0093b3d0d257573cce80e1e27731f76
IshitaBera14 <156918112+IshitaBera14@users.noreply.github.com>
2024-11-11 03:11:54 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail ="ishita@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+//accountId =  2// not allowed 
+accountEmail ="hc@hc.com"
+accountPassword = "121212"
+accountCity = "vapi"
+
+
+
+console.log(accountId)
+console.table([accountEmail , accountPassword , accountCity , accountId , accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"user strict"; // treat all JS code in newer version 
+
+// alert ( 3 + 3 ) // we are using node js not browser 
+
+console.log(3+3)
+console.log("hello")
+
+let name = "Ishita"
+let age = 21
+let isLoggedIn = false
+let state ;
+
+// number => 2 to power 53
+// bigint
+// String =>""
+// boolean => true/false
+// null =?standalone value 
+// undefined 
+// symbol => unique
+
+// object 
+
+console.log(typeof "Ishita")    // string
+console.log(typeof age)        // number
+console.log(typeof null)       // object
+console.log(typeof undefined) // undefined
