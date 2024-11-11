53c962ae51b4c3a03e75f383d3e552310b20399b
IshitaBera14 <156918112+IshitaBera14@users.noreply.github.com>
2024-11-11 12:21:39 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,26 @@
+// 2 (primitive )(non primitive)
+
+// primitive (total 7)
+
+// non primitive/reference 
+
+// array object function
+
+const heros = ["a" , "b" , "c"] //array
+
+let myobj=     //object
+{
+    name: "ishita",
+    age: 18,
+}
+
+//function(){}
+
+const myFunction = function()
+{
+    console.log("hello")
+}
+
+console.log(typeof myFunction)
+console.log(typeof myobj)
+console.log(typeof heros)
